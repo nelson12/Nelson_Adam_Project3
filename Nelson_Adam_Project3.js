787d18cfed6b447b0ae7fb89b24cd9d38c272a50
nelson12# <aw.nelson@hotmail.com>
2014-04-24 19:52:32 -0400

    variables

---
+++ Nelson_Adam_Project3.js
@@ -0,0 +1,9 @@
+//Adam Nelson Scalable Data Infrastructures 4/24/2014
+
+var gameYourPlaying = "World of Warcraft";
+var races = 14;
+var classes = 11;
+var yourRace = "Human";
+var yourClassIsHunter = true;
+var maleOrFemale = "Male";
+
