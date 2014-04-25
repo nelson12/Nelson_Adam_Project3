62eed3288d5c749ddce0e64bc5565e8a6687633e
nelson12# <aw.nelson@hotmail.com>
2014-04-24 20:00:17 -0400

    variable outputs

---
+++ Nelson_Adam_Project3 copy.js
@@ -0,0 +1,14 @@
+//Adam Nelson Scalable Data Infrastructures 4/24/2014
+
+var gameYourPlaying = "World of Warcraft";
+var races = 14;
+var classes = 11;
+var yourRace = "Human";
+var yourClassIsHunter = true;
+var maleOrFemale = "Male";
+
+console.log("You are playing" + gameYourPlaying);
+console.log("You will be a"+ yourRace);
+console.log("I hear that there are" + races + "races in the game and" + classes + "to choose from.");
+console.log("So you are going to play a Hunter, Right?");
+
